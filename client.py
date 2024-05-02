d38c04c909e048e663aff0d27967e4f38723f189
Vrushank Gunjur <vrushankg@hotmail.com>
2024-05-01 21:14:57 -0700

    simple client/server

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+
+def run_client():
+    host = socket.gethostname()
+    port = 5001
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+    client_socket.send("Hello from client".encode())
+    print(client_socket.recv(1024))
+
+if __name__ == "__main__":
+    run_client()
